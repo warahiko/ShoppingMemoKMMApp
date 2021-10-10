b5bd17e6d07cf30a406492c82afe246ab07f2cd7
go shibata <warahiko@gmail.com>
2021-10-10 23:19:42 +0900

    first commit

---
+++ shared/src/commonMain/kotlin/io/github/warahiko/shoppingmemokmmapplication/Greeting.kt
@@ -0,0 +1,7 @@
+package io.github.warahiko.shoppingmemokmmapplication
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, Shopping memo app in ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/io/github/warahiko/shoppingmemokmmapplication/Platform.kt
@@ -0,0 +1,5 @@
+package io.github.warahiko.shoppingmemokmmapplication
+
+expect class Platform() {
+    val platform: String
+}
