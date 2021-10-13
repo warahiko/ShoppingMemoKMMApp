1b7dc95ce18c00d848b402b0c2a8ec1e2fe39057
go shibata <warahiko@gmail.com>
2021-10-13 23:27:10 +0900

    [ios] AppDelegate でKoin 初期化

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  iosApp
+//
+//  Created by 柴田 剛 on 2021/10/13.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import UIKit
+import shared
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        KoinKt.doInitKoinIos()
+        return true
+    }
+}
