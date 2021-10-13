8ec7ea0c35591ebb97f9721afa81d212fb01772e
go shibata <warahiko@gmail.com>
2021-10-13 23:12:35 +0900

    [Android] Koin の初期化をApplication#onCreate で実行

---
+++ androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/ShoppingMemoApplication.kt
@@ -0,0 +1,18 @@
+package io.github.warahiko.shoppingmemokmmapplication.android
+
+import android.app.Application
+import io.github.warahiko.shoppingmemokmmapplication.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+
+class ShoppingMemoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin {
+            androidLogger()
+            androidContext(this@ShoppingMemoApplication)
+        }
+    }
+}
