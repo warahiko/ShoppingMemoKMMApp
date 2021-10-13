901fcb98049dc2b3e29a466f0a940a4b283a72f1
go shibata <warahiko@gmail.com>
2021-10-13 23:14:23 +0900

    [shared] ios 用の初期化関数、Injection のためのラッパー追加

--- shared/src/commonMain/kotlin/io/github/warahiko/shoppingmemokmmapplication/Koin.kt
+++ shared/src/commonMain/kotlin/io/github/warahiko/shoppingmemokmmapplication/Koin.kt
@@ -2,6 +2,8 @@ package io.github.warahiko.shoppingmemokmmapplication
 
 import io.github.warahiko.shoppingmemokmmapplication.data.repository.ShoppingItemListRepository
 import org.koin.core.KoinApplication
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
@@ -16,4 +18,12 @@ fun initKoin(appDeclaration: KoinAppDeclaration = {}): KoinApplication {
 
 private val modules: Module = module {
     single { ShoppingItemListRepository("Api") }
+}
+
+@Suppress("unused")
+fun initKoinIos() = initKoin {}
+
+@Suppress("unused")
+class InjectorIos : KoinComponent {
+    val shoppingItemListRepository: ShoppingItemListRepository by inject()
 }
