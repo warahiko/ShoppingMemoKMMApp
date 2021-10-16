07edbbcfdc14d5983808f4ad0e2381b0f46f6bc1
go shibata <warahiko@gmail.com>
2021-10-16 16:53:20 +0900

    Application にErrorMonitor をLifecycleCallback として設定

--- androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/di/Koin.kt
+++ androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/di/Koin.kt
@@ -1,5 +1,10 @@
 package io.github.warahiko.shoppingmemokmmapplication.android.di
 
+import io.github.warahiko.shoppingmemokmmapplication.android.error.ErrorCatcher
+import io.github.warahiko.shoppingmemokmmapplication.android.error.ErrorHolder
+import io.github.warahiko.shoppingmemokmmapplication.android.error.ErrorMonitor
+import io.github.warahiko.shoppingmemokmmapplication.android.error.LaunchSafe
+import io.github.warahiko.shoppingmemokmmapplication.android.error.LaunchSafeImpl
 import io.github.warahiko.shoppingmemokmmapplication.android.ui.shoppingitem.list.ShoppingItemListScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
@@ -7,4 +12,11 @@ import org.koin.dsl.module
 
 val androidModules: Module = module {
     viewModel { ShoppingItemListScreenViewModel(get()) }
+}
+
+val androidErrorHandlingModules: Module = module {
+    single { ErrorHolder() }
+    single { ErrorCatcher(get()) }
+    single { ErrorMonitor(get()) }
+    single<LaunchSafe> { LaunchSafeImpl(get()) }
 }
