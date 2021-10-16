ad6a39b2dc641ba8eabc5e1b0a40391bb67a8877
go shibata <warahiko@gmail.com>
2021-10-16 16:25:08 +0900

    material theme 設定用ファイル追加

---
+++ androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package io.github.warahiko.shoppingmemokmmapplication.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightGreen = Color(0xFF9CCC65)
+val Green = Color(0xFF8BC34A)
+val LightRed = Color(0xFFFF8A65)
