7d88a6cedb7edfc5373a97d08c09486feb856926
go shibata <warahiko@gmail.com>
2021-10-16 16:34:01 +0900

    最近使ったアプリでの見た目を変更

--- androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/MainActivity.kt
+++ androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/MainActivity.kt
@@ -1,8 +1,10 @@
 package io.github.warahiko.shoppingmemokmmapplication.android
 
+import android.app.ActivityManager
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.color.MaterialColors
 import io.github.warahiko.shoppingmemokmmapplication.android.ui.ShoppingMemoNavHost
 import io.github.warahiko.shoppingmemokmmapplication.android.ui.theme.ShoppingMemoAppTheme
 
@@ -11,10 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setRecentAppsAppearance()
+
         setContent {
             ShoppingMemoAppTheme {
                 ShoppingMemoNavHost()
             }
         }
+    }
+
+    /**
+     * 最近使ったアプリでの見た目を変更する
+     */
+    private fun setRecentAppsAppearance() {
+        @Suppress("Deprecation")
+        val taskDescription = ActivityManager.TaskDescription(
+            null,
+            null,
+            MaterialColors.getColor(this, R.attr.colorPrimarySurface, 0),
+        )
+        setTaskDescription(taskDescription)
     }
 }
