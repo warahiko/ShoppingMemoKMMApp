cb6fb734682d628a96f747fa3347a8f26601c52f
go shibata <warahiko@gmail.com>
2021-10-16 14:05:40 +0900

    compose お試し

--- androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/MainActivity.kt
+++ androidApp/src/main/java/io/github/warahiko/shoppingmemokmmapplication/android/MainActivity.kt
@@ -1,11 +1,20 @@
 package io.github.warahiko.shoppingmemokmmapplication.android
 
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import io.github.warahiko.shoppingmemokmmapplication.data.repository.ShoppingItemRepository
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +23,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        lifecycleScope.launch {
-            val tv: TextView = findViewById(R.id.text_view)
-            tv.text =
-                shoppingItemRepository.fetchShoppingItems().take(5).fold("") { l, r ->
-                    "$l, $r"
-                }
+        setContent {
+            MainScreen(shoppingItemRepository)
         }
+    }
+}
+
+@Composable
+private fun MainScreen(
+    shoppingItemRepository: ShoppingItemRepository,
+) {
+    val shoppingItems by shoppingItemRepository.shoppingItems.collectAsState()
+    val text by remember(shoppingItems) {
+        derivedStateOf {
+            shoppingItems?.take(5)
+                ?.joinToString {
+                    it.toString()
+                }
+                ?: ""
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text,
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
+
+    LaunchedEffect(Unit) {
+        shoppingItemRepository.fetchShoppingItems()
     }
 }
