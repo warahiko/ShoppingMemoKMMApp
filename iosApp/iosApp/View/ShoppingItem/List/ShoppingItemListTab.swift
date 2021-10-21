9b0bae33a015ac2d529a9d318eb2dc3dda166161
go shibata <warahiko@gmail.com>
2021-10-21 23:42:14 +0900

    タブのenum としてShoppingItemListTab 追加

---
+++ iosApp/iosApp/View/ShoppingItem/List/ShoppingItemListTab.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingItemListTab.swift
+//  iosApp
+//
+//  Created by 柴田 剛 on 2021/10/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+// CaseIterable プロトコルに準拠させることで allCases プロパティが生える
+enum ShoppingItemListTab: Int, CaseIterable, Identifiable {
+    case main
+    case archived
+    case deleted
+    
+    var id: Int {
+        return self.rawValue
+    }
+}
