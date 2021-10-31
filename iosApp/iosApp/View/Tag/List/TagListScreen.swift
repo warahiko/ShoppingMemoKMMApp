c79bb083bbf7ff2511269bec7b26fbe0dd624549
go shibata <warahiko@gmail.com>
2021-10-31 17:27:07 +0900

    TagListScreen 追加

---
+++ iosApp/iosApp/View/Tag/List/TagListScreen.swift
@@ -0,0 +1,21 @@
+//
+//  TagListScreen.swift
+//  iosApp
+//
+//  Created by 柴田 剛 on 2021/10/31.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        TagListScreen()
+    }
+}
