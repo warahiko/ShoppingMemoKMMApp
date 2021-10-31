66620deede37d556c70479631d6959a2b8b1fa29
go shibata <warahiko@gmail.com>
2021-10-31 16:47:03 +0900

    アイテム追加処理にローディング画面追加

--- iosApp/iosApp/View/ShoppingItem/Add/AddShoppingItemViewModel.swift
+++ iosApp/iosApp/View/ShoppingItem/Add/AddShoppingItemViewModel.swift
@@ -13,33 +13,36 @@ extension AddShoppingItemScreen {
         let tagRepository = InjectorIos().tagRepository
         let tagIosRepository = InjectorIos().tagIosRepository
         let addShoppingItemUseCase = InjectorIos().addShoppingItemUseCase
-        
+
         @Published var uiModel: UiModel = .EMPTY
-        
+        @Published var showProgress: Bool = false
+
         init() {
             tagIosRepository.tags.collect { tags in
                 self.uiModel = UiModel.from(tags: tags as? [Tag])
             }
         }
-        
+
         func addShoppingItem(_ shoppingItem: ShoppingItem, onComplete: @escaping () -> Void) {
+            self.showProgress = true
             addShoppingItemUseCase.invoke(shoppingItem: shoppingItem) { _, error in
                 if error == nil {
                     onComplete()
                 }
+                self.showProgress = false
             }
         }
     }
-    
+
     struct UiModel {
         let tagGroupedByType: Dictionary<String, [Tag]>
-        
+
         static let EMPTY: UiModel = .init(tagGroupedByType: [:])
-        
+
         static func from(tags: [Tag]?) -> UiModel {
             let tagGroupedByType = tags.orEmpty()
                 .groupBy { tag in tag.type }
-            
+
             return UiModel(tagGroupedByType: tagGroupedByType)
         }
     }
