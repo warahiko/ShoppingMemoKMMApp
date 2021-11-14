f4059f592107ac2b140f94100386eecc877797b2
go shibata <warahiko@gmail.com>
2021-11-14 17:01:32 +0900

    EditShoppingItemScreen 追加

---
+++ iosApp/iosApp/View/ShoppingItem/Edit/EditShoppingItemScreen.swift
@@ -0,0 +1,25 @@
+//
+//  EditShoppingItemScreen.swift
+//  iosApp
+//
+//  Created by 柴田 剛 on 2021/10/31.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditShoppingItemScreen: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("アイテムを編集")
+                .navigationBarTitleDisplayMode(NavigationBarItem.TitleDisplayMode.inline)
+        }
+    }
+}
+
+struct EditShoppingItemScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        EditShoppingItemScreen()
+    }
+}
